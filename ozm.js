48ba08ca43a6976608228cd89ed1a35058b18a0e
Sorrybro802 <poweshuxratov@gmail.com>
2021-12-23 15:32:45 +0500

    Month-kun yoki tun qilish mumkin

---
+++ ozm.js
@@ -0,0 +1,34 @@
+function ertalab() {
+    let bgcolor = document.getElementById("sections")
+    bgcolor.style.background = "white"
+
+    let white1 = document.getElementById("white")
+    white1.style.display = "none"
+
+    let black1 = document.getElementById("black")
+    black1.style.display = "block"
+
+    let textcolor = document.getElementById("textcolor")
+    textcolor.style.color = "black"
+
+    let month = document.getElementById("month")
+    month.style.color = "black"
+}
+
+function kechasi() {
+    let bgcolor2 = document.getElementById("sections")
+    bgcolor2.style.background = "black"
+
+    let block2 = document.getElementById("black")
+    block2.style.display = "none"
+
+    let white2 = document.getElementById("white")
+    white2.style.display = "block"
+
+    let textcolor2 = document.getElementById("textcolor")
+    textcolor2.style.color = "white"
+
+    let month2 = document.getElementById("month")
+    month2.style.color = "white"
+}
+
